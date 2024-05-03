c7e87c4f42c41958ef534e8009898c78b16d1eec
nyinyi979 <nyinyi095062687@gmail.com>
2024-05-03 18:35:44 +0630

    frameworks

--- app/_components/frameworks.tsx
+++ app/_components/frameworks.tsx
@@ -1,16 +1,17 @@
 "use client"
 import React from "react"
 import {motion} from "framer-motion";
-import { SkillViewer } from "./img_viewer";
 export default function Skills(){
     const skills = React.useRef({
         frontEnd: ["HTML","CSS","SCSS","Javascript","Typescript","React","React native"],
-        backEnd: ["Node","Bun","Express","Fastify","Elysia","MySQL","Postgres","MongoDB","PrismaORM","Redux","Redux Persist"],
+        backEnd: ["Node","Bun","Express","Fastify","Elysia"],
+        data: ["MySQL","Postgres","MongoDB","PrismaORM","Redux","Redux Persist","Tanstack Query","RN Async Storage","Local Storage"],
         frameworks: ["NextJS","Expo","Framer motion","SwiperJS","ChartJS"],
         others: ["Photoshop","Figma","Github"]
     })
     const frontEle = "w-fit px-3 py-2 m-2 bg-blue-100 shadow shadow-md shadow-blue-300 hover:bg-blue-200 hover:rounded-md cursor-pointer duration-300";
     const backEle = "w-fit px-3 py-2 m-2 bg-cyan-100 shadow shadow-md shadow-cyan-300 hover:bg-cyan-200 hover:rounded-md cursor-pointer duration-300";
+    const dataEle = "w-fit px-3 py-2 m-2 bg-amber-100 shadow shadow-md shadow-amber-300 hover:bg-amber-200 hover:rounded-md cursor-pointer duration-300";
     const frameEle = "w-fit px-3 py-2 m-2 bg-emerald-100 shadow shadow-md shadow-emerald-300 hover:bg-emerald-200 hover:rounded-md cursor-pointer duration-300";
     const otherEle = "w-fit px-3 py-2 m-2 bg-purple-100 shadow shadow-md shadow-purple-300 hover:bg-purple-200 hover:rounded-md cursor-pointer duration-300";
 
@@ -25,7 +26,7 @@ export default function Skills(){
                 >
                     Skills (Have fun dragging)
                 </motion.h1>
-                <h2 className="text-2xl text-underline text-blue-500 hover:text-blue-600 duration-300">Front end</h2>
+                <h2 className="my-2 text-2xl text-underline text-blue-500 hover:text-blue-600 duration-300">Front end</h2>
                 <div className="flex flex-wrap gap-5">
                     {skills.current.frontEnd.map((a)=>
                         <motion.div 
@@ -39,7 +40,7 @@ export default function Skills(){
                         </motion.div>
                     )}
                 </div>
-                <h2 className="text-2xl text-underline text-cyan-500 hover:text-cyan-600 duration-300">Back end</h2>
+                <h2 className="my-2 text-2xl text-underline text-cyan-500 hover:text-cyan-600 duration-300">Back end</h2>
                 <div className="flex flex-wrap gap-5">
                     {skills.current.backEnd.map((a)=>
                         <motion.div 
@@ -52,7 +53,20 @@ export default function Skills(){
                         </motion.div>
                     )}
                 </div>
-                <h2 className="text-2xl text-underline text-emerald-500 hover:text-emerald-600 duration-300">Frameworks</h2>
+                <h2 className="my-2 text-2xl text-underline text-amber-500 hover:text-amber-600 duration-300">Datastore</h2>
+                <div className="flex flex-wrap gap-5">
+                    {skills.current.data.map((a)=>
+                        <motion.div 
+                            key={a}
+                            drag
+                            dragSnapToOrigin
+                            whileDrag={{scale:1.2,borderRadius:6,backgroundColor:"rgb(252 211 77)"}}
+                            className={dataEle}>
+                                {a}
+                        </motion.div>
+                    )}
+                </div>
+                <h2 className="my-2 text-2xl text-underline text-emerald-500 hover:text-emerald-600 duration-300">Frameworks</h2>
                 <div className="flex flex-wrap gap-5">
                     {skills.current.frameworks.map((a)=>
                         <motion.div
@@ -65,7 +79,7 @@ export default function Skills(){
                         </motion.div>
                     )}
                 </div>
-                <h2 className="text-2xl text-underline text-purple-500 hover:text-purple-600 duration-300">Others</h2>
+                <h2 className="my-2 text-2xl text-underline text-purple-500 hover:text-purple-600 duration-300">Others</h2>
                 <div className="flex flex-wrap gap-5">
                     {skills.current.others.map((a)=>
                         <motion.div 
