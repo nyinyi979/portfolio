ffd5d63b7141ae6096e70f41a4f746edfa8e2097
nyinyi979 <nyinyi095062687@gmail.com>
2024-05-22 17:32:31 +0630

    name

--- app/_components/name.tsx
+++ app/_components/name.tsx
@@ -7,7 +7,7 @@ export default function Name(){
     let [pos, setPos] = React.useState({x:200,y:400})
     React.useEffect(()=>{
         document.getElementById("name_box")!.addEventListener("mousemove",(ev)=>{
-            setPos({x:ev.screenX-200, y: ev.screenY-300})
+            setPos({x:ev.screenX-100, y: ev.screenY-200})
         })
     })            
     return(
